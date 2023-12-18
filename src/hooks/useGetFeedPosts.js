f5e900d284bfcd4a53a80178c4e7222023021cde
nikhildevnikhil <nikhildeonikhil@gmail.com>
2023-12-18 17:09:21 +0530

    deploy now

--- src/hooks/useGetFeedPosts.js
+++ src/hooks/useGetFeedPosts.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import usePostStore from "../store/postStore";
 import useAuthStore from "../store/authStore";
 import useShowToast from "./useShowToast";
@@ -7,42 +7,43 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 
 const useGetFeedPosts = () => {
-    const [isLoading,setIsLoading] = useState(true);
-    const {posts,setPosts} = usePostStore();
-    const authUser = useAuthStore(state => state.user);
-    const showToast = useShowToast();
-    const {setUserProfile} = userProfileStore();
+  const [isLoading, setIsLoading] = useState(true);
+  const { posts, setPosts } = usePostStore();
+  const authUser = useAuthStore((state) => state.user);
+  const showToast = useShowToast();
+  const { setUserProfile } = userProfileStore();
 
-    useEffect(() => {
-        
-        const getFeedPosts = async () => {
-            setIsLoading(true);
-            if(authUser.following.length === 0){
-                setIsLoading(false);
-                setPosts([]);
-                return;
-            }
-            const q = query(collection(firestore,"posts"),where("createdBy","in",authUser.following));
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setIsLoading(true);
+      if (authUser.following.length === 0) {
+        setIsLoading(false);
+        setPosts([]);
+        return;
+      }
+      const q = query(
+        collection(firestore, "posts"),
+        where("createdBy", "in", authUser.following)
+      );
 
-            try {
-                const querySnapshot = await getDocs(q);
-                const feedPosts = [];
-                querySnapshot.forEach((doc) => {
-                    feedPosts.push({id : doc.id,...doc.data()})
-                } )
-                feedPosts.sort((a,b) => b.createdAt - a.createdAt);
-                setPosts(feedPosts);
-                
-            } catch (error) {
-                showToast("Error",error.message,"error");
-            }finally{
-                setIsLoading(false);
-            }
-        }
-        if(authUser) getFeedPosts();
-    },[authUser,showToast,setPosts,setUserProfile])
+      try {
+        const querySnapshot = await getDocs(q);
+        const feedPosts = [];
+        querySnapshot.forEach((doc) => {
+          feedPosts.push({ id: doc.id, ...doc.data() });
+        });
+        feedPosts.sort((a, b) => b.createdAt - a.createdAt);
+        setPosts(feedPosts);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (authUser) getFeedPosts();
+  }, [authUser, showToast, setPosts, setUserProfile]);
 
-  return {isLoading,posts};
-}
+  return { isLoading, posts };
+};
 
-export default useGetFeedPosts
+export default useGetFeedPosts;
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,12 +1,12 @@
 import { useSignOut } from "react-firebase-hooks/auth";
-import { auth } from "../firebase/Firebase";
+import { auth } from "../firebase/firebase";
 import useShowToast from "./useShowToast";
 import useAuthStore from "../store/authStore";
 
 const useLogout = () => {
   const [signOut, isLoggingOut, error] = useSignOut(auth);
   const showToast = useShowToast();
-  const logoutUser = useAuthStore(state => state.logout);
+  const logoutUser = useAuthStore((state) => state.logout);
   const handleLogout = async () => {
     try {
       await signOut();
